9b0722f4fe638d79e488f139a54287544bec6820
MengLinMaker <menglinmaker@gmail.com>
2026-07-27 18:01:31 +1000

    fix: PEMs do not require decoding again

--- terraform/cluster/providers.tf
+++ terraform/cluster/providers.tf
@@ -1,17 +1,17 @@
 provider "helm" {
   kubernetes = {
     host                   = module.talos.kubeconfig_data.host
-    client_certificate     = base64decode(module.talos.kubeconfig_data.client_certificate)
-    client_key             = base64decode(module.talos.kubeconfig_data.client_key)
-    cluster_ca_certificate = base64decode(module.talos.kubeconfig_data.cluster_ca_certificate)
+    client_certificate     = module.talos.kubeconfig_data.client_certificate
+    client_key             = module.talos.kubeconfig_data.client_key
+    cluster_ca_certificate = module.talos.kubeconfig_data.cluster_ca_certificate
   }
 }
 
 provider "kubectl" {
   host                   = module.talos.kubeconfig_data.host
-  client_certificate     = base64decode(module.talos.kubeconfig_data.client_certificate)
-  client_key             = base64decode(module.talos.kubeconfig_data.client_key)
-  cluster_ca_certificate = base64decode(module.talos.kubeconfig_data.cluster_ca_certificate)
+  client_certificate     = module.talos.kubeconfig_data.client_certificate
+  client_key             = module.talos.kubeconfig_data.client_key
+  cluster_ca_certificate = module.talos.kubeconfig_data.cluster_ca_certificate
   lazy_load              = true
   load_config_file       = false
 }
